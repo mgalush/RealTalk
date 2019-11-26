5218df97f63ccf7405bd46d7ee398050d929d466
Melissa Galush <galushmn@gmail.com>
2019-11-26 17:26:43 -0500

    move mongo logic to its own module

--- src/app.js
+++ src/app.js
@@ -1,39 +1,18 @@
 
 const express = require('express');
 const app = express();
+const database = require('./database')
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
 const send404 = function(req, res) {
     res.status(404).render('404');
 };
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
- 
-// Database Name
-const dbName = 'RealTalk';
-let client;
-let collection;
-// Use connect method to connect to the server
-MongoClient.connect(url, { useUnifiedTopology: true }, function(err, c) {
-  assert.equal(null, err);
-  console.log("Connected successfully to mongodb server");
-  client = c;
- 
-  const db = client.db(dbName);
-
-  collection = db.collection(dbName);
-});
-
 app.get('/', function(req, res){
-    collection.distinct('category', function(error, categories) {
+    database().distinct('category', function(error, categories) {
         res.render('index', {categories: categories.sort()});
     });
 });
@@ -57,7 +36,7 @@ app.get('/topic/:category/:topicNumber([1-9]\\d{0,})', function(req, res){
     viewModel.next = topicNumber + 1;
 
     // query mongo for a topic, skipping by the number specified in the url
-    collection.findOne({category: category}, {skip: topicNumber-1}, function(error, topic) {
+    database().findOne({category: category}, {skip: topicNumber-1}, function(error, topic) {
         if(!topic) {
             send404(req, res);
             return;
@@ -78,7 +57,6 @@ const server = app.listen(3000, function(){
 });
 
 function exitHandler() {
-    if (client) client.close(); console.log('closing mongo client');
     if (server) server.close(function() {
         console.log('express server closed');
     });
---
+++ src/database.js
@@ -0,0 +1,43 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'RealTalk';
+const collectionName = 'RealTalk';
+
+let collection;
+// Use connect method to connect to the server
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to mongodb server");
+
+    function exitHandler() {
+        if (client) {
+            client.close();
+            console.log('closing mongo client');
+        }
+    }
+    //do something when app is closing
+    process.on('exit', exitHandler);
+    //catches ctrl+c event
+    process.on('SIGINT', exitHandler);
+    // catches "kill pid" (for example: nodemon restart)
+    process.on('SIGUSR1', exitHandler);
+    process.on('SIGUSR2', exitHandler);
+    //catches uncaught exceptions
+    process.on('uncaughtException', exitHandler);
+
+    const db = client.db(dbName);
+
+    collection = db.collection(collectionName);
+});
+
+module.exports = function() {
+    if (!collection) {
+        throw('mongo collection is not defined');
+    }
+    return collection;
+}
