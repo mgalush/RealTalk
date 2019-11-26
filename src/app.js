f5647c8bb49bf37f4cadd82115c90e5cc0ebd6e4
Melissa Galush <galushmn@gmail.com>
2019-11-25 20:43:31 -0500

    404 page

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ app.set('view engine', 'ejs');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+const send404 = function(req, res) {
+    res.status(404).send('404 bitch');
+};
+
 // Connection URL
 const url = 'mongodb://localhost:27017';
  
@@ -46,12 +50,16 @@ app.get('/friends/:topicNumber([1-9]\\d{0,})', function(req, res){
 
     // query mongo for a topic, skipping by the number specified in the url
     collection.findOne({}, {skip: topicNumber-1}, function(error, topic) {
+        if(!topic) {
+            send404(req, res);
+            return;
+        }
 
         viewModel.topic = topic;
 
         // render the friends ejs template and pass in variables
         res.render('friends', viewModel);
-    })
+    });
 });
 
 app.get('/family', function(req, res){
@@ -63,9 +71,7 @@ app.get('/relationship', function(req, res){
 });
 
 // return 404 for all non matching routes
-app.get('*', function(req, res) {
-    res.send('404 bitch', 404);
-})
+app.get('*', send404);
 
 const server = app.listen(3000, function(){
     console.log('Express server is listening');
