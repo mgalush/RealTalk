8ed560b668de95840981a361719ee22278b7e07c
Melissa Galush <galushmn@gmail.com>
2019-11-26 17:36:36 -0500

    move routes to its own module

--- src/app.js
+++ src/app.js
@@ -1,56 +1,11 @@
 
 const express = require('express');
 const app = express();
-const database = require('./database')
 
 app.use(express.static(__dirname + '/public'));
-
 app.set('view engine', 'ejs');
 
-const send404 = function(req, res) {
-    res.status(404).render('404');
-};
-
-app.get('/', function(req, res){
-    database().distinct('category', function(error, categories) {
-        res.render('index', {categories: categories.sort()});
-    });
-});
-
-app.get("/topic/:category", function (req, res) {
-    res.redirect(`${req.url}/1`)
-});
-
-// url for /<some category>/<some number greater than zero>
-app.get('/topic/:category/:topicNumber([1-9]\\d{0,})', function(req, res){
-
-    // get the category
-    const category = req.params.category;
-    
-    // get the number of this page
-    const topicNumber = parseInt(req.params.topicNumber);
-
-    // calculate back and number href links
-    const viewModel = {};
-    viewModel.back = topicNumber - 1;
-    viewModel.next = topicNumber + 1;
-
-    // query mongo for a topic, skipping by the number specified in the url
-    database().findOne({category: category}, {skip: topicNumber-1}, function(error, topic) {
-        if(!topic) {
-            send404(req, res);
-            return;
-        }
-
-        viewModel.topic = topic;
-
-        // render the friends ejs template and pass in variables
-        res.render('category', viewModel);
-    });
-});
-
-// return 404 for all non matching routes
-app.get('*', send404);
+require('./routes')(app);
 
 const server = app.listen(3000, function(){
     console.log('Express server is listening');
---
+++ src/routes.js
@@ -0,0 +1,48 @@
+const database = require('./database');
+
+function send404(req, res) {
+    res.status(404).render('404');
+};
+
+module.exports = (app) => {
+    app.get('/', function(req, res){
+        database().distinct('category', function(error, categories) {
+            res.render('index', {categories: categories.sort()});
+        });
+    });
+    
+    app.get("/topic/:category", function (req, res) {
+        res.redirect(`${req.url}/1`)
+    });
+    
+    // url for /<some category>/<some number greater than zero>
+    app.get('/topic/:category/:topicNumber([1-9]\\d{0,})', function(req, res){
+    
+        // get the category
+        const category = req.params.category;
+        
+        // get the number of this page
+        const topicNumber = parseInt(req.params.topicNumber);
+    
+        // calculate back and number href links
+        const viewModel = {};
+        viewModel.back = topicNumber - 1;
+        viewModel.next = topicNumber + 1;
+    
+        // query mongo for a topic, skipping by the number specified in the url
+        database().findOne({category: category}, {skip: topicNumber-1}, function(error, topic) {
+            if(!topic) {
+                send404(req, res);
+                return;
+            }
+    
+            viewModel.topic = topic;
+    
+            // render the friends ejs template and pass in variables
+            res.render('category', viewModel);
+        });
+    });
+    
+    // return 404 for all non matching routes
+    app.get('*', send404);
+}
