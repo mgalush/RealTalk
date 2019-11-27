c922d2ef890727f23bf419be46ac7558acc49bc5
Melissa Galush <galushmn@gmail.com>
2019-11-26 20:44:37 -0500

    routes module returns router object instead of setting directly on app

--- src/app.js
+++ src/app.js
@@ -3,15 +3,13 @@ const express = require('express');
 const app = express();
 const processUtil = require('./util/process_util')
 
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || 3000);
-
-require('./routes')(app);
-
-const server = app.listen(app.get('port'), () => {
-    console.log('[express] server listening');
-});
+const server = app.use(express.static(__dirname + '/public'))
+                  .use(require('./routes'))
+                  .set('view engine', 'ejs')
+                  .set('port', process.env.PORT || 3000)
+                  .listen(app.get('port'), () => {
+                      console.log('[express] server listening');
+                  });
 
 processUtil.onExit(() => {
     if (server) {
--- src/routes.js
+++ src/routes.js
@@ -1,56 +1,57 @@
 const database = require('./database');
+const routes = new require('express').Router();
 
 function send404(req, res) {
     res.status(404).render('404');
 };
 
-module.exports = (app) => {
-    app.get('/', (req, res) => {
-        database().distinct('category', (error, categories) => {
-            res.render('index', {categories: categories.sort()});
-        });
+routes.get('/', (req, res) => {
+    database().distinct('category', (error, categories) => {
+        res.render('index', {categories: categories.sort()});
     });
-    
-    app.get("/topic/:category", (req, res) => {
-        res.redirect(`${req.url}/1`)
-    });
-    
-    // url for /<some category>/<some number greater than zero>
-    app.get('/topic/:category/:pageNumber([1-9]\\d{0,})', async (req, res) => {
-    
-        // get the category
-        const category = req.params.category;
-        
-        // get the number of this page
-        const pageNumber = parseInt(req.params.pageNumber);
-    
-        // query mongo for a topic and the one after it, skipping by the number specified in the url
-        const query = {
-            category: category
-        };
-        const options = {
-            skip: pageNumber - 1,
-            limit: 2
-        }
-        const cursor = database().find(query, options);
+});
 
-        // 404 if we don't have any topics
-        if (!await cursor.count()) {
-            send404(req, res);
-            return;
-        }
+routes.get("/topic/:category", (req, res) => {
+    res.redirect(`${req.url}/1`)
+});
 
-        // calculate back and next href links
-        const viewModel = {
-            topic: await cursor.next(),
-            previous: pageNumber - 1,
-            next: await cursor.hasNext() ? pageNumber + 1 : 0,
-        };
+// url for /<some category>/<some number greater than zero>
+routes.get('/topic/:category/:pageNumber([1-9]\\d{0,})', async (req, res) => {
 
-        // render the category ejs template and pass in view model
-        res.render('category', viewModel);
-    });
+    // get the category
+    const category = req.params.category;
     
-    // return 404 for all non matching routes
-    app.get('*', send404);
-}
+    // get the number of this page
+    const pageNumber = parseInt(req.params.pageNumber);
+
+    // query mongo for a topic and the one after it, skipping by the number specified in the url
+    const query = {
+        category: category
+    };
+    const options = {
+        skip: pageNumber - 1,
+        limit: 2
+    }
+    const cursor = database().find(query, options);
+
+    // 404 if we don't have any topics
+    if (!await cursor.count()) {
+        send404(req, res);
+        return;
+    }
+
+    // calculate back and next href links
+    const viewModel = {
+        topic: await cursor.next(),
+        previous: pageNumber - 1,
+        next: await cursor.hasNext() ? pageNumber + 1 : 0,
+    };
+
+    // render the category ejs template and pass in view model
+    res.render('category', viewModel);
+});
+
+// return 404 for all non matching routes
+routes.get('*', send404);
+
+module.exports = routes;
